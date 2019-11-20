da6c76c8baf031ba8252fbc0292efbee88a4ff66
perronet <arnad.1997@gmail.com>
2019-11-20 12:39:32 +0100

    Syscall skeleton

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,19 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+// static void halt (void) NO_RETURN;
+// static void exit (int status) NO_RETURN;
+// static pid_t exec (const char *file);
+// static int wait (pid_t);
+// static bool create (const char *file, unsigned initial_size);
+// static bool remove (const char *file);
+// static int open (const char *file);
+// static int filesize (int fd);
+// static int read (int fd, void *buffer, unsigned length);
+// static int write (int fd, const void *buffer, unsigned length);
+// static void seek (int fd, unsigned position);
+// static unsigned tell (int fd);
+// static void close (int fd);
 
 void
 syscall_init (void) 
@@ -18,3 +31,68 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+// static void halt ()
+// {
+
+// }
+
+// static void exit (int status)
+// {
+
+// }
+
+// static pid_t exec (const char *file)
+// {
+//   return 0;
+// }
+
+// static int wait (pid_t)
+// {
+//   return 0;
+// }
+
+// static bool create (const char *file, unsigned initial_size)
+// {
+//   return 0;
+// }
+
+// static bool remove (const char *file)
+// {
+//   return 0;
+// }
+
+// static int open (const char *file)
+// {
+//   return 0;
+// }
+
+// static int filesize (int fd)
+// {
+//   return 0;
+// }
+
+// static int read (int fd, void *buffer, unsigned length)
+// {
+//   return 0;
+// }
+
+// static int write (int fd, const void *buffer, unsigned length)
+// {
+//   return 0;
+// }
+
+// static void seek (int fd, unsigned position)
+// {
+
+// }
+
+// static unsigned tell (int fd)
+// {
+//   return 0;
+// }
+
+// static void close (int fd)
+// {
+
+// }
