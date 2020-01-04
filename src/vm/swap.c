7119d077bfa946c8c9e0285b7b0f5f542db1c539
mmaida <00marcomaida00@gmail.com>
2020-01-04 12:03:52 +0100

    Added swap lock and fs synch on lock

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,6 +1,7 @@
 #include "devices/block.h"
 #include "threads/vaddr.h"
 #include "threads/synch.h"
+#include "filesys/fsaccess.h"
 #include "swap.h"
 #include <bitmap.h>
 #include <stdbool.h>
@@ -11,56 +12,69 @@
 struct block  *swap_device;
 
 static struct bitmap *swap_bm;
+struct lock swap_lock;
 
 void 
 swap_init ()
 {
-	swap_device = block_get_role (BLOCK_SWAP);
-	ASSERT (swap_device != NULL);
-	size_t bmsize = block_size(swap_device) / SECTORS_PER_PAGE;
-	swap_bm = bitmap_create(bmsize);
-	ASSERT (swap_bm != NULL);
+  swap_device = block_get_role (BLOCK_SWAP);
+  ASSERT (swap_device != NULL);
+  size_t bmsize = block_size(swap_device) / SECTORS_PER_PAGE;
+  swap_bm = bitmap_create(bmsize);
+  ASSERT (swap_bm != NULL);
 
-	bitmap_set_all(swap_bm, true);
-}	
+  bitmap_set_all(swap_bm, true);
+} 
 
 void 
 swap_in (size_t slot, void* page)
 {
-	size_t swap_addr_base = slot * SECTORS_PER_PAGE;
-	for (size_t i = 0; i < SECTORS_PER_PAGE; i++)
-		{
-			block_sector_t from = swap_addr_base + i;
-			void* to = page + i * BLOCK_SECTOR_SIZE;
+    lock_acquire (&swap_lock);
+  size_t swap_addr_base = slot * SECTORS_PER_PAGE;
+  for (size_t i = 0; i < SECTORS_PER_PAGE; i++)
+    {
+      block_sector_t from = swap_addr_base + i;
+      void* to = page + i * BLOCK_SECTOR_SIZE;
+      lock_fs ();
+      block_read (swap_device, from, to);
+      unlock_fs ();
+    }
 
-			block_read (swap_device, from, to);
-		}
-
-	swap_free (slot);
+  swap_free (slot);
+    lock_release (&swap_lock);
 }
 
 size_t 
 swap_out (const void* page)
 {
-	size_t slot = bitmap_scan_and_flip (swap_bm, 0, 1, true);
+  lock_acquire (&swap_lock);
+  size_t slot = bitmap_scan_and_flip (swap_bm, 0, 1, true);
 
-	if (slot != BITMAP_ERROR)
-		{
-			size_t swap_addr_base = slot * SECTORS_PER_PAGE;
-			for (size_t i = 0; i < SECTORS_PER_PAGE; i++)
-				{
-					const void* from = page + i * BLOCK_SECTOR_SIZE;
-					block_sector_t to = swap_addr_base + i;
-					block_write (swap_device, to, from);
-				}
-			return slot;	
-		}
-	else
-		return SWAP_ERROR;
+  if (slot != BITMAP_ERROR)
+    {
+      size_t swap_addr_base = slot * SECTORS_PER_PAGE;
+      lock_fs ();
+      for (size_t i = 0; i < SECTORS_PER_PAGE; i++)
+        {
+          const void* from = page + i * BLOCK_SECTOR_SIZE;
+          block_sector_t to = swap_addr_base + i;
+          block_write (swap_device, to, from);
+        }
+      unlock_fs ();
+      lock_release (&swap_lock);
+      return slot;  
+    }
+  else
+    {
+      lock_release (&swap_lock);
+      return SWAP_ERROR;
+    }
 }
 
 void swap_free(size_t slot)
 {
-	bitmap_flip (swap_bm, slot);
+  lock_acquire (&swap_lock);
+  bitmap_flip (swap_bm, slot);
+  lock_release (&swap_lock);
 }
-	
+  
