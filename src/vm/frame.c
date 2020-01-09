24beae05ed888fe2d66ab47d6b6445e95f850092
perronet <arnad.1997@gmail.com>
2020-01-09 15:47:45 +0100

    Revert

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -12,14 +12,14 @@ static struct lock frame_hash_lock;
 unsigned find_frame (const struct hash_elem *e, void *aux UNUSED)
 {
   struct frame_entry *frame = hash_entry (e, struct frame_entry, elem);
-  return hash_bytes (&frame->kpage, sizeof (frame->kpage));
+  return hash_bytes (&frame->page, sizeof (frame->page));
 }
 
 bool compare_frame (const struct hash_elem *e1, const struct hash_elem *e2, void *aux UNUSED)
 {
   struct frame_entry *frame1 = hash_entry (e1, struct frame_entry, elem);
   struct frame_entry *frame2 = hash_entry (e2, struct frame_entry, elem);
-  return frame1->kpage < frame2->kpage;
+  return frame1->page < frame2->page;
 }
 
 void vm_frame_alloc_init ()
@@ -31,6 +31,7 @@ void vm_frame_alloc_init ()
 void *vm_frame_alloc (enum palloc_flags flags, void *thread_vaddr)
 {
   void *page = NULL;
+
   /* Must request allocation from the user pool, kernel memory is non-pageable */
   if (flags & PAL_USER)
     page = palloc_get_page (flags);
@@ -45,11 +46,8 @@ void *vm_frame_alloc (enum palloc_flags flags, void *thread_vaddr)
     {
       struct frame_entry *f = evict_and_get_frame();
       ASSERT (f != NULL);
-      struct owner_info *own_info = malloc (sizeof (struct owner_info));
-      own_info->owner = thread_current ();
-      own_info->upage = thread_vaddr;
-      list_push_back (&f->owners, &own_info->elem);
-      page = f->kpage;
+      f->thread_vaddr = thread_vaddr;
+      page = f->page;
     }
 
   return page;
@@ -60,37 +58,33 @@ void vm_frame_free (void *page)
   struct hash_elem *e;
   struct frame_entry find;
 
-  find.kpage = page;
+  find.page = page;
   /* Remove from list */
   lock_acquire (&frame_hash_lock);
   e = hash_find (&frame_hash, &find.elem);
   hash_delete (&frame_hash, e);
   lock_release (&frame_hash_lock);
 
-  struct frame_entry *remove = hash_entry (e, struct frame_entry, elem);
-  free_owners (remove);
-  free (remove);
+  free (hash_entry (e, struct frame_entry, elem));
+  /* Free the page */
   palloc_free_page (page);
 }
 
 bool frame_hash_add (void *page, enum palloc_flags flags, void *thread_vaddr)
 {
   struct frame_entry *frame = malloc (sizeof(struct frame_entry));
-  struct owner_info *own_info = malloc (sizeof(struct owner_info));
 
   if (frame == NULL)
     return false;
 
-  frame->kpage = page;
-  own_info->owner = thread_current ();
-  own_info->upage = thread_vaddr;
-  list_init (&frame->owners);
-  list_push_back (&frame->owners, &own_info->elem);
-
+  frame->page = page;
+  frame->owner = thread_current();
+  frame->thread_vaddr = thread_vaddr;
   lock_acquire (&frame_hash_lock);
 
-  if (flags & PAL_USER)
+  if (flags & PAL_USER){
     hash_insert (&frame_hash, &frame->elem);
+  }
 
   lock_release (&frame_hash_lock);
 
@@ -99,15 +93,19 @@ bool frame_hash_add (void *page, enum palloc_flags flags, void *thread_vaddr)
 
 struct frame_entry * evict_and_get_frame()
 {
+  struct thread *t = thread_current ();
+
   lock_acquire (&frame_hash_lock);
 
   struct frame_entry *victim = select_frame_to_evict();
   if (victim == NULL)
     PANIC ("No frame to evict");
 
+  /* Save old frame before modifying the frame entry */
   if (!page_out_evicted_frame (victim))
     PANIC ("Can't page out evicted frame");
 
+  victim->owner = t;
   lock_release (&frame_hash_lock);
 
   return victim;
@@ -126,8 +124,12 @@ struct frame_entry * select_frame_to_evict()
     while (hash_next (&i))
     {
       f = hash_entry (hash_cur (&i), struct frame_entry, elem);
-      /* If no owner has accessed the page, then evict */
-      if (!pagedir_check_accessed_foreach_owner (f))
+      if (pagedir_is_accessed (f->owner->pagedir, f->thread_vaddr))
+      {
+        /* Give second chance */
+        pagedir_set_accessed (f->owner->pagedir, f->thread_vaddr, false);
+      }
+      else
       {
         found = true;
         break;
@@ -140,41 +142,31 @@ struct frame_entry * select_frame_to_evict()
 /* Only call with lock acquired */
 bool page_out_evicted_frame (struct frame_entry *f)
 {
-  bool is_shared = (list_size (&f->owners) > 1);
-  struct list_elem *e = list_begin (&f->owners);
-  struct owner_info *own_info = list_entry (e, struct owner_info, elem);
-  struct pt_suppl_entry *pt_entry = pt_suppl_get (&own_info->owner->pt_suppl, own_info->upage);
+  struct pt_suppl_entry *pt_entry = pt_suppl_get (&f->owner->pt_suppl, f->thread_vaddr);
   size_t swap_slot_id;
 
-  if (pt_entry == NULL)
-    {// Lazy loaded -> swap out
-      swap_slot_id = swap_out (own_info->upage);
-      if ((int)swap_slot_id == SWAP_ERROR){
-        PANIC ("Cannot swap");
-        return false;
+  if (pt_entry == NULL) //if (IS_LAZY (pt_entry->status))
+    {// Lazy loaded
+      //if (pagedir_is_dirty (f->owner->pagedir, f->thread_vaddr))
+      {// If dirty -> put in swap memory
+        swap_slot_id = swap_out (f->thread_vaddr);
+        if ((int)swap_slot_id == SWAP_ERROR){
+          PANIC ("Cannot swap");
+          return false;
+        }
       }
-
-      for (e = list_begin (&f->owners); e != list_end (&f->owners); e = list_next (e))
-      {
-        own_info = list_entry (e, struct owner_info, elem);
-        pt_entry = pt_suppl_get (&own_info->owner->pt_suppl, own_info->upage);
-        ASSERT (pt_entry == NULL);
-
-        pt_entry = malloc (sizeof (struct pt_suppl_entry));
-        pt_entry->vaddr = own_info->upage;
-        pt_entry->swap_slot = swap_slot_id;
-        pt_entry->file_info = NULL;
-        SET_TYPE(pt_entry->status, LAZY);
-        SET_PRESENCE (pt_entry->status, SWAPPED);
-        hash_insert (&own_info->owner->pt_suppl, &pt_entry->elem);
-        pagedir_clear_page (own_info->owner->pagedir, own_info->upage);
-      }
-      free_owners (f);
+      pt_entry = malloc (sizeof (struct pt_suppl_entry));
+      pt_entry->vaddr = f->thread_vaddr;
+      pt_entry->swap_slot = swap_slot_id;
+      pt_entry->file_info = NULL;
+      SET_TYPE(pt_entry->status, LAZY);
+      SET_PRESENCE (pt_entry->status, SWAPPED);
+      hash_insert (&f->owner->pt_suppl, &pt_entry->elem);
+      pagedir_clear_page (f->owner->pagedir, f->thread_vaddr);
     }
   else if (IS_MMF (pt_entry->status))
     {// MMF -> write back to file if dirty
-      ASSERT (!is_shared);
-      if(pagedir_is_dirty (own_info->owner->pagedir, pt_entry->vaddr))
+      if(pagedir_is_dirty (f->owner->pagedir, pt_entry->vaddr))
       {
         lock_fs ();
         file_write_at (pt_entry->file_info->file, pt_entry->vaddr, 
@@ -183,45 +175,12 @@ bool page_out_evicted_frame (struct frame_entry *f)
       }
       SET_TYPE(pt_entry->status, MMF);
       SET_PRESENCE (pt_entry->status, UNLOADED);
-      pagedir_clear_page (own_info->owner->pagedir, pt_entry->vaddr);
+      pagedir_clear_page (f->owner->pagedir, pt_entry->vaddr);
     }
   else
     {
-      PANIC ("STATUS of page %p of %d is %d\n", pt_entry->vaddr, own_info->owner->tid, pt_entry->status);
+      PANIC ("STATUS of page %p of %d is %d\n", pt_entry->vaddr, f->owner->tid, pt_entry->status);
     }
 
   return true;
-}
-
-bool pagedir_check_accessed_foreach_owner (struct frame_entry *f)
-{
-  struct list_elem *e;
-  bool accessed = false;
-
-  for (e = list_begin (&f->owners); e != list_end (&f->owners); e = list_next (e))
-    {
-      struct owner_info *own_info = list_entry (e, struct owner_info, elem);
-
-      /* Update access bits */
-      if (pagedir_is_accessed (own_info->owner->pagedir, own_info->upage)){
-        accessed = true;
-        pagedir_set_accessed (own_info->owner->pagedir, own_info->upage, false);
-      }
-    }
-
-  return accessed;
-}
-
-void free_owners (struct frame_entry *f)
-{
-  struct list_elem *e = list_begin (&f->owners);
-
-  while (e != list_end (&f->owners))
-  {
-    struct list_elem *remove = e;
-    struct owner_info *own_info_remove = list_entry (remove, struct owner_info, elem);
-    e = list_next (e);
-    list_remove (remove);
-    free (own_info_remove);
-  }
 }
